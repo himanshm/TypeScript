e99b40fd74130ff8aa3f28e71528afb0959d043b
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-13 16:12:48 +0530

    Unknown and Never types Changes to be committed: 	modified:   1 First-Project/app.js 	new file:   1 First-Project/unknown-never.ts

--- 1 First-Project/unknown-never.ts
+++ 1 First-Project/unknown-never.ts
@@ -1,15 +1,24 @@
 // Unknown type
-var userInput;
-var userName;
+let userInput: unknown;
+let userName: string;
+
 userInput = 5;
 userInput = 'Max';
+
 // userName = userInput; //Type 'unknown' is not assignable to type 'string'.
+
 if (typeof userInput === 'string') {
-    userName = userInput;
+  userName = userInput;
 }
+
 // So, I need such an extra type check here with unknown to be able to assign a unknown value to a value with a fixed type
+
 // never type - never is another type functions can return
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
 }
+
 generateError('An Error Occured!', 500);
+
+// The above function never return anything
