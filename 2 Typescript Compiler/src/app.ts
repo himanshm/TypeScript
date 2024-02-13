661f50788f4a0a1db7bdd00fec27d7506a17fd91
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-13 17:34:19 +0530

    rootDir and outDir

---
+++ 2 Typescript Compiler/src/app.ts
@@ -0,0 +1,4 @@
+// To activate watch mode tsc app.ts --watch or tsc app.ts -w and quit it using CTRL + C after development
+
+// If you have multiple TypeScript files in your project Run tsc --init and then tsx -w to run all the files
+
