e078868a2e4946b1408ee475f084ae0b74c503dc
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-14 17:29:40 +0530

    Inheritance Changes to be committed: 	modified:   4 Classes & Interfaces/src/app.ts

--- 4 Classes & Interfaces/src/app.ts
+++ 4 Classes & Interfaces/src/app.ts
@@ -3,24 +3,14 @@
 class Department {
   //   private id: string;
   //   private name: string;
-  private employees: string[] = []; // Make this array accessible only from inside the class public modifier is default
-  // Private and public however aren't known to JavaScript, but only to typescript
-
-  // Besides properties, we can also mark methods as private
-
-  // Functions in objects are simply called methods
-  // Constructor is essentially a function tied to this class and tied to any object we create based on the class which is executed when the object is being created. This allows us to do some initiallisation work for the object being built.
-
-  // If we have certain fields that shouldn't not just be private or public but they shouldn't change after their initialisation => readonly This also doesn't exist in javascript and is introduced by TypeScript
+  private employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.name = n;
   }
 
   describe(this: Department) {
-    // When describe() is executed, `this` inside of the describe should always refer to an instance that's based on the Department class so an object that would be of type Department
     console.log(`Department (${this.id}): ${this.name}`);
-    // this refers to the concrete instance of this Department class that was created and with dot(.) notation we can access all the properties and methods of this instance
   }
 
   addEmployee(employee: string) {
@@ -33,18 +23,37 @@ class Department {
   }
 }
 
-const accounting = new Department('d1', 'Accounting');
+// Inheritance => the class which inherits automatically gets everything the base class, Department in this case, has
+// As long as we don't add a dedicated constructor for this inherited class, for this subclass, the base class' constructor is automatically used when we instantiate our subclass
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT'); //  calls the constructor of the base class
+    this.admins = admins;
+  }
+}
 
-accounting.addEmployee('Max');
-accounting.addEmployee('Manu');
-// accounting.employees[2] = 'Anna'; // Property 'employees' is private and only accessible within class 'Department'.
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting'); //  calls the constructor of the base class
+  }
 
-console.log(accounting);
+  addReport(text: string) {
+    this.reports.push(text);
+  }
 
-accounting.describe();
-accounting.printEmplyeeInfo();
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment('d1', ['Himanshu']);
+
+it.addEmployee('Max');
+it.addEmployee('Manu');
+
+console.log(it);
+
+it.describe();
+it.printEmplyeeInfo();
 // RULE OF THUMB: this refers to the thing which is responsible for calling the method
-
-// const accountingCopy = { name: 's', describe: accounting.describe };
-
-// accountingCopy.describe();
