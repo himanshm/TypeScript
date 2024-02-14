1e4cb5c71195fa59528b1c0a1abb05662339954a
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-14 16:42:26 +0530

    Private and Public

--- 4 Classes & Interfaces/src/app.ts
+++ 4 Classes & Interfaces/src/app.ts
@@ -2,6 +2,10 @@
 
 class Department {
   name: string;
+    private employees: string[] = []; // Make this array accessible only from inside the class public modifier is default
+    // Private and public however aren't known to JavaScript, but only to typescript
+
+  // Besides properties, we can also mark methods as private
 
   // Functions in objects are simply called methods
   // Constructor is essentially a function tied to this class and tied to any object we create based on the class which is executed when the object is being created. This allows us to do some initiallisation work for the object being built.
@@ -15,15 +19,29 @@ class Department {
     console.log('Department: ' + this.name);
     // this refers to the concrete instance of this Department class that was created and with dot(.) notation we can access all the properties and methods of this instance
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmplyeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department('Accounting');
 
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+// accounting.employees[2] = 'Anna'; // Property 'employees' is private and only accessible within class 'Department'.
+
 console.log(accounting);
 
 accounting.describe();
+accounting.printEmplyeeInfo();
 // RULE OF THUMB: this refers to the thing which is responsible for calling the method
 
-const accountingCopy = { name: 's', describe: accounting.describe };
+// const accountingCopy = { name: 's', describe: accounting.describe };
 
-accountingCopy.describe();
+// accountingCopy.describe();
