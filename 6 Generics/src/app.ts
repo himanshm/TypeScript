3080247a2f8a8a340487fbda44fc3fa00f2ebc64
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-15 16:57:27 +0530

    Utility Types

--- 6 Generics/src/app.ts
+++ 6 Generics/src/app.ts
@@ -43,3 +43,29 @@ const numberStorage = new DataStorage<number>();
 // objStorage.removeItem({ name: 'Vim' });
 
 // console.log(objStorage.getItems());
+
+// Generic Utility Types
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+
+  //  partial kind of wraps our own type, and changes it to a type where all these properties are optional.
+
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Max', 'Anna']; // Now this array cannot be changed
