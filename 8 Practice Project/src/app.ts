3b8060b9bc6e12f178750cf3e8ecfa3a88b26c43
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-17 19:01:03 +0530

    Interact with DOM elements

--- 8 Practice Project/src/app.ts
+++ 8 Practice Project/src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = <HTMLTemplateElement>(
@@ -13,9 +16,34 @@ class ProjectInput {
       this.templateElement.content,
       true
     );
-    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element = importedNode.firstElementChild as HTMLFormElement; // form element
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement; // Typescript has no chance of understanding that the query selector is going to return an input element here. So to tell typescript, I will again use casting, and cast this to an input element,
+
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
+  }
+
+  /*The problem here is that, this here, that this keyword in submitHandler does not point at the class actually. Why? Well because of the way JavaScript and typescript works, we bind the method here to the event listener and when we bind something to an event or with the help of an event listener then that method, which is going to get executed, will have this bound to something else, in this case to the current target of the event. So this in this method will not point at the class, when the method is triggered upon an event with an event listener. Now the work around or the solution is to actually call bind here on submitHandler to preconfigure how this function is going execute when it executes in the future, and the first argument we can pass to bind then is actually what the this keyword will refer to inside of the to be executed function. */
 
   private attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
