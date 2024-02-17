51e33a2f2ce1588938547a1215e2d686a70e487c
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-17 15:12:25 +0530

    Decorator Factory

--- 7 Decorators/src/app.ts
+++ 7 Decorators/src/app.ts
@@ -5,6 +5,8 @@ function Logger(logString: string) {
   };
 }
 
+/*A decorator factory is basically returns a decorator function, but allows us to configure it when we assign it as a decorator to something. */
+
 @Logger('LOGGING - PERSON')
 
 /*So the decorator runs when JavaScript finds your class definition, your constructor function definition. Not when you use that constructor function to instantiate an object. */
