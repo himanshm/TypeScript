dd8319abfc22b792e528da8a6e2e665aea36b4c2
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-17 16:50:17 +0530

    Create and autobind Decorator

--- 7 Decorators/src/app.ts
+++ 7 Decorators/src/app.ts
@@ -8,16 +8,24 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
   console.log('TEMPLATE FACTORY');
-  return function (constructor: any) {
-    console.log('Rendering template');
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector('h1')!.textContent = p.name;
-    }
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log('Rendering template');
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector('h1')!.textContent = this.name;
+        }
+      }
+    };
   };
 }
+
+// TypeScript only accepts return values on class, method and accessor decorators
 
 // @Logger('LOGGING - PERSON')
 @Logger('LOGGING')
@@ -90,3 +98,38 @@ class Product {
     return this._price * (1 + tax);
   }
 }
+
+const p1 = new Product('Book', 19);
+const p2 = new Product('Book 2', 29);
+
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      /*what does this refer to in here? Keep in mind it's inside of this getter method so this will refer to whatever is responsible for triggering this getter method. the getter method will be triggered by the concrete object to which it belongs, so this inside of the getter method will always refer to the object on which we defined the getter. This will not be overwritten by addEventListener because the getter is like an extra layer between our function that's being executed and the object to which it belongs */
+      return boundFn;
+    },
+  };
+  return adjustedDescriptor;
+}
+
+class Printer {
+  message: string;
+
+  constructor(message: string) {
+    this.message = message;
+  }
+
+  @AutoBind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer('This Works');
+
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage);
