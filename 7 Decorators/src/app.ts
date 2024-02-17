7ff22cd133f0e504a1c5f283d8e09f94ffc4f012
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-17 17:56:23 +0530

    Validation with Decorators

--- 7 Decorators/src/app.ts
+++ 7 Decorators/src/app.ts
@@ -25,8 +25,6 @@ function WithTemplate(template: string, hookId: string) {
   };
 }
 
-// TypeScript only accepts return values on class, method and accessor decorators
-
 // @Logger('LOGGING - PERSON')
 @Logger('LOGGING')
 @WithTemplate('<h1>My Person Object</h1>', 'app')
@@ -102,34 +100,105 @@ class Product {
 const p1 = new Product('Book', 19);
 const p2 = new Product('Book 2', 29);
 
-function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
-  const adjustedDescriptor: PropertyDescriptor = {
+  const adjDescriptor: PropertyDescriptor = {
     configurable: true,
     enumerable: false,
     get() {
       const boundFn = originalMethod.bind(this);
-      /*what does this refer to in here? Keep in mind it's inside of this getter method so this will refer to whatever is responsible for triggering this getter method. the getter method will be triggered by the concrete object to which it belongs, so this inside of the getter method will always refer to the object on which we defined the getter. This will not be overwritten by addEventListener because the getter is like an extra layer between our function that's being executed and the object to which it belongs */
       return boundFn;
     },
   };
-  return adjustedDescriptor;
+  return adjDescriptor;
 }
 
 class Printer {
-  message: string;
+  message = 'This works!';
 
-  constructor(message: string) {
-    this.message = message;
-  }
-
-  @AutoBind
+  @Autobind
   showMessage() {
     console.log(this.message);
   }
 }
 
-const p = new Printer('This Works');
+const p = new Printer();
+p.showMessage();
 
 const button = document.querySelector('button')!;
 button.addEventListener('click', p.showMessage);
+
+// ---
+
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProp: string]: string[]; // ['required', 'positive']
+  };
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['required'],
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['positive'],
+  };
+}
+
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) {
+    return true;
+  }
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const titleEl = document.getElementById('title') as HTMLInputElement;
+  const priceEl = document.getElementById('price') as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+
+  if (!validate(createdCourse)) {
+    alert('Invalid input, please try again!');
+    return;
+  }
+  console.log(createdCourse);
+});
